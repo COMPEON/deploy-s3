cad580b3b3646cd6dadbc0deb7fbf678e9095c4b
SplittyDev <splittydev@gmail.com>
2021-07-01 16:46:24 +0200

    Add environment helper test coverage

---
+++ test/helpers/validation.js
@@ -0,0 +1,71 @@
+const expect = require('chai').expect
+const sinon = require('sinon')
+
+const { makeValidator } = require('../../src/helpers')
+
+const core = require('@actions/core')
+
+describe('helpers/validation', () => {
+    describe('#makeValidator', () => {
+        let getInputStub
+
+        beforeEach(() => {
+            getInputStub = sinon.stub(core, 'getInput')
+        })
+
+        afterEach(() => {
+            getInputStub.restore()
+        })
+
+        it('should validate required parameters', () => {
+            // given
+            const requiredInputs = ['pg-login', 'pg-pass']
+            const optionalInputs = []
+            getInputStub.withArgs('pg-login').returns('admin')
+            getInputStub.withArgs('pg-pass').returns('supersecret')
+            // when
+            const params = makeValidator(requiredInputs, optionalInputs)
+            // then
+            expect(params).to.deep.equal({
+                pgLogin: 'admin',
+                pgPass: 'supersecret',
+            })
+        })
+
+        it('should throw on non-existing required parameters', () => {
+            // given
+            const requiredInputs = ['pg-login', 'pg-pass']
+            const optionalInputs = []
+            getInputStub.withArgs('pg-login').returns('admin')
+            getInputStub.withArgs('pg-pass').throws()
+            // when
+            const fn = () => makeValidator(requiredInputs, optionalInputs)
+            // then
+            expect(fn).to.throw()
+        })
+
+        it('should validate existing optional parameters', () => {
+            // given
+            const requiredInputs = []
+            const optionalInputs = ['pg-schema']
+            getInputStub.withArgs('pg-schema').returns('unicorns')
+            // when
+            const params = makeValidator(requiredInputs, optionalInputs)
+            // then
+            expect(params).to.deep.equal({
+                pgSchema: 'unicorns',
+            })
+        })
+
+        it('should validate non-existing optional parameters', () => {
+            // given
+            const requiredInputs = []
+            const optionalInputs = []
+            getInputStub.withArgs('pg-schema').returns('unicorns')
+            // when
+            const params = makeValidator(requiredInputs, optionalInputs)
+            // then
+            expect(params).to.deep.equal({})
+        })
+    })
+})
